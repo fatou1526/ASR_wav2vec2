6c02c2581533217cb6c2392cf56b8a85ce8c1c67
Fatou <ndeyefatousene1502@gmail.com>
2023-09-25 10:30:43 +0000

    Loading the model for fastAPI use

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import torch
-
+import os
 class ASRInference:
-    def __init__(self, model_name = 'Fatou/customWav2Vec2Model'):
+    def __init__(self, model_name = 'Fatou/customwolof'):
         self.model = Wav2Vec2ForCTC.from_pretrained(model_name)
-        self.tokenizer = Wav2Vec2Processor.from_pretrained(model_name)
+        self.processor = Wav2Vec2Processor.from_pretrained(model_name)
 
     def inference(self, audio):
         inputs = self.processor(audio, sampling_rate = 16000, return_tensors = 'pt')
